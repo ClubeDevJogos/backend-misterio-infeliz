4d8d04e6d7a3284a54869a92dd8166af22b0711f
GustavoGebhardt <gustavorgebhardt@gmail.com>
2024-09-18 15:35:41 -0300

    First Commit

---
+++ src/config/database.ts
@@ -0,0 +1,9 @@
+const databaseConfig = {
+    username: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DB_DIALECT as 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql'
+}
+
+export default databaseConfig
