fb40990b737b53c095f964af9a43be2ba6cb80c4
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-09-21 16:22:13 -0300

    user session route

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,8 @@
 import { z } from "zod";
+import jwt from "jsonwebtoken";
+
+import authConfig from "../config/auth";
+import checkUserPassword from "../services/checkUserPassword";
 
 import user from "../models/user";
 
@@ -33,6 +37,37 @@ class UserController {
     });
 
     return res.send();
+  }
+
+  async session(req: any, res: any) {
+    const userSchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    if (userSchema.safeParse(req.body).success === false) {
+      return res.status(400).json({ error: "Invalid data" });
+    }
+
+    const { email } = req.body;
+
+    const userExists = await user.findOne({ where: { email } });
+
+    if (!userExists) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const { password } = req.body;
+
+    if (!(await checkUserPassword(password, userExists))) {
+      return res.status(400).json({ error: "Incorrect password" });
+    }
+
+    return res.json({
+      token: jwt.sign({}, authConfig.secret ?? "", {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,6 @@ import userController from "../controllers/userController";
 const routes = Router();
 
 routes.post("/user", userController.store);
+routes.post("/session", userController.session);
 
 export default routes;
